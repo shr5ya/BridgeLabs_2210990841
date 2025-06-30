d24f4673209a2781a486fea4471c8f6716f6337e
shr5ya <s8reya@gmail.com>
2025-06-30 12:03:16 +0530

    bleh

---
+++ MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass{
+    int b=1; // instance variable b is initialized to 1
+    static int s;
+
+    public void n1() {
+        system.out.println(a);
+        system.out.println(b);
+        system.out.println(s);
+    }
+public static void main(String[] args) {
+        int a;
+
+        
+    }
+
+}
