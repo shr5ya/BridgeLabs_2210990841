6efc47ef5c007fa8eeb92dbc66bd36cdaa53b9c0
shr5ya <s8reya@gmail.com>
2025-07-02 12:37:43 +0530

    2/07/2025

--- CalculatorScanner.java
+++ CalculatorScanner.java
@@ -1,15 +1,38 @@
 import java.util.Scanner;
 
+//calculater
 public class CalculatorScanner {
-    public static void main(String[] args) {
+    public void add(int a,int b){
+        int result=a+b;
+        System.out.println(result);
+
+    }
+    public void sub(int a,int b){
+        int result=a-b;
+        System.out.println(result);
+
+    }
+    public void mul(int a,int b){
+        int result=a*b;
+        System.out.println(result);
+
+    }
+    public void div(int a,int b){
+        int result=a/b;
+        System.out.println(result);
+
+    }
+    public static void main(String[]args){
+        System.out.println("Shreya");
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter number");
-        int input = sc.nextInt();
-
-        System.out.println("enter second number");
-        int b= sc.nextInt();
-
-        int result = a+b;
-        sc.close();
+        System.out.print("Enter first number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int y = sc.nextInt();
+        CalculatorScanner m=new CalculatorScanner();
+        m.add(x,y);
+        m.sub(x,y);
+        m.mul(x,y);
+        m.div(x,y);
     }
 }
