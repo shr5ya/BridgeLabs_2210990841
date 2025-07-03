ee5a0ce1012d8c476366edfc6084fe545a1cab7d
shr5ya <s8reya@gmail.com>
2025-07-03 11:32:14 +0530

    3/07/2025

---
+++ CODE/FirstProgram.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class FirstProgram {
+        public static void main(String[] args)
+        {
+            System.out.println("shreya");
+            Scanner sc=new Scanner(System.in);
+        }
+    }
