e5300295641b8516adbf68ccda05b43a81632596
shr5ya <s8reya@gmail.com>
2025-07-03 12:37:45 +0530

    3/07/2025

---
+++ CODE/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 6;
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b; 
+        int remainder = a % b; 
+
+       
+        System.out.println("The addition of a and b is :" + addition);
+        System.out.println("The subtraction of a and b is :" + subtraction);
+        System.out.println("The multiplication of a and b is : " + multiplication);
+        System.out.println("The division of a and b is :" + division);
+        System.out.println("The remainder of a and b is :" + remainder);
+    }
+}
